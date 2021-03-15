f172f98bcbfd6bb6e2daad3674d7f18c6269effe
Josh Chamberlain <josh@getwhiplash.com>
2021-03-15 10:46:32 -0400

    analytics

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -31,6 +31,17 @@ export default function Layout(props) {
               content="We gather data, then educate citizens and empower the public. PDAP aggregates and consolidates open-source, law-enforcement activity data into digestible records."
             ></meta>
             <title>{data.site.siteMetadata.title}</title>
+            {/* Global site tag (gtag.js) - Google Analytics */}
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-REKS6B95BL"></script>
+              <script>
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-REKS6B95BL');
+              `}
+              </script>
           </Helmet>
           <Header pages={props.pages} />
           <main className="container">{props.children}</main>
