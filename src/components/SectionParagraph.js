acceeac858f97beba95daedd46072fbf81f5b43e
Josh Chamberlain <josh@getwhiplash.com>
2021-08-28 18:27:22 -0400

    most of it
    removing gatsby, adding the HTML site

--- src/components/SectionParagraph.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import * as sectionStyles from "../css/pageSection.module.css"
-
-export default function SectionParagraph({ paragraph }) {
-  return (
-    <p className={sectionStyles.section__item__paragraph} dangerouslySetInnerHTML={{ __html: paragraph.text }}></p>
-  )
-}
