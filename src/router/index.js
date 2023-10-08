233fe591df0d163137cd03d34c0de28a5f2238f3
Ben Polinsky <benjamin.polinsky@gmail.com>
2023-10-08 11:15:29 -0400

    add vue
    reverted a bunch of stuff; un-reverting

---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from "vue-router";
+import AboutView from "@/views/AboutView.vue";
+import DataSourcesView from "@/views/DataSourcesView.vue";
+import DonateView from "@/views/DonateView.vue";
+import HomeView from "@/views/HomeView.vue";
+import JobsView from "@/views/JobsView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: AboutView,
+    },
+    {
+      path: "/donate",
+      name: "donate",
+      component: DonateView,
+    },
+    {
+      path: "/data-sources",
+      name: "data-sources",
+      component: DataSourcesView,
+    },
+    {
+      path: "/jobs",
+      name: "jobs",
+      component: JobsView,
+    },
+
+    {
+      path: "/docs-and-resources",
+      name: "docsAndResources",
+      component: HomeView,
+    },
+  ],
+  linkActiveClass: "current"
+});
+
+export default router;
