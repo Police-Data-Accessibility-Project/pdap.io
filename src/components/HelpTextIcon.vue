2c9f24785d0cbbdf2aad5f633b573baa8ed654a8
Josh Chamberlain <josh@getwhiplash.com>
2023-11-03 13:39:19 -0400

    help text icon component

---
+++ src/components/HelpTextIcon.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="help-text-icon">
+    <a @click="toggleTooltip">
+      <i class="fa fa-question-circle"></i>
+    </a>
+    <div class="tooltip" v-if="showTooltip">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showTooltip: false,
+    };
+  },
+  methods: {
+    toggleTooltip() {
+      this.showTooltip = !this.showTooltip;
+      console.log("Tooltip visibility:", this.showTooltip);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.help-text-icon {
+  display: inline-block;
+  position: relative;
+}
+
+.tooltip {
+  position: absolute;
+  top: 100%; /* Positions the tooltip below the icon */
+  left: 0; /* Aligns the tooltip to the left edge of the icon */
+  z-index: 100; /* Ensures the tooltip layers above other content */
+  transform: translateY(10px); /* Adds a small space between the icon and tooltip */
+  white-space: nowrap;
+  padding:8px;
+  border:1px solid black;
+  font-size:20px;
+  /* Additional styles for appearance, padding, colors, etc., can be added here */
+}
+</style>
--- src/views/CaseStudiesView.vue
+++ src/views/CaseStudiesView.vue
@@ -1,8 +1,12 @@
+<script setup>
+import HelpTextIcon from '@/components/HelpTextIcon.vue';
+</script>
+
 <template>
   <div class="section">
     <div class="container">
       <h1>Case Studies</h1>
-      <p>Here are our case studies. <i class="fa fa-question-circle"></i></p>
+      <p>Here are our case studies. <help-text-icon>Slotted content</help-text-icon></p>
     </div>
   </div>
 </template>
