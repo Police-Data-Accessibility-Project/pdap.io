609efbb41b52cb58cb8af43d265d2c81bee9d9aa
Joshua Graber <joshua.d.graber@gmail.com>
2023-11-28 11:35:35 -0500

    styles(jobs): add containers to jobs view

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,46 @@
-import { createRouter, createWebHistory } from "vue-router";
-import AboutView from "@/views/AboutView.vue";
-import DataView from "@/views/DataView.vue";
-import DonateView from "@/views/DonateView.vue";
-import HomeView from "@/views/HomeView.vue";
-import JobsView from "@/views/JobsView.vue";
-import VolunteerView from "@/views/VolunteerView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import AboutView from '@/views/AboutView.vue';
+import DataView from '@/views/DataView.vue';
+import DonateView from '@/views/DonateView.vue';
+import HomeView from '@/views/HomeView.vue';
+import JobsView from '@/views/JobsView.vue';
+import VolunteerView from '@/views/VolunteerView.vue';
 
 const router = createRouter({
-  history: createWebHistory("/"),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: AboutView,
-    },
-    {
-      path: "/donate",
-      name: "donate",
-      component: DonateView,
-    },
-    {
-      path: "/data",
-      name: "data",
-      component: DataView,
-    },
-    {
-      path: "/jobs",
-      name: "jobs",
-      component: JobsView,
-    },
-    {
-      path: "/volunteer",
-      name: "volunteer",
-      component: VolunteerView,
-    },
-  ],
-  linkActiveClass: "current"
+	history: createWebHistory('/'),
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: HomeView,
+		},
+		{
+			path: '/about',
+			name: 'about',
+			component: AboutView,
+		},
+		{
+			path: '/donate',
+			name: 'donate',
+			component: DonateView,
+		},
+		{
+			path: '/data',
+			name: 'data',
+			component: DataView,
+		},
+		{
+			path: '/jobs',
+			name: 'jobs',
+			component: JobsView,
+		},
+		{
+			path: '/volunteer',
+			name: 'volunteer',
+			component: VolunteerView,
+		},
+	],
+	linkActiveClass: 'current',
 });
 
 export default router;
