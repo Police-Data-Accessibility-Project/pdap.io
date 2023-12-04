b833e346365f05ad7f80c1abef8077bbbf929399
Josh Chamberlain <josh@getwhiplash.com>
2023-12-04 13:39:01 -0500

    add tailwind

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { FlexContainer, GridContainer, GridItem } from 'pdap-design-system';
 import 'pdap-design-system/styles';
+import './style.css'
 
 import { createApp } from 'vue';
 import App from './App.vue';
