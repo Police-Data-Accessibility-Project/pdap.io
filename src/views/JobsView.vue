2617960a802c293566e9054e8db01a6334e35849
Josh Chamberlain <josh@getwhiplash.com>
2024-02-16 15:04:25 -0500

    jobs update

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -4,7 +4,6 @@
 			<div>
 				<h1>Jobs at PDAP</h1>
 				<h3>Open positions</h3>
-				<a href="#regional-data-experts">Regional data experts</a><br />
 				<a href="#software-development-opportunities">Software development opportunities</a><br />
 				<h3>How to apply</h3>
 				<p>
@@ -37,78 +36,11 @@
 					</p>
 				</div>
 				<div class="mt-12">
-					<h2 id="regional-data-experts">Regional Data Experts</h2>
-					<h4><i class="fa fa-info-circle"></i> Remote, temporary</h4>
-					<p>
-						Each police jurisdiction has different data practices. We work best when we have a PDAP
-						ally embedded in a county or city data ecosystem, using our tools to track Data Sources,
-						Volunteers, and Requests in the region.
-					</p>
-					<p>
-						We are looking for people with a personal interest in the transparency of their local
-						legal system.
-					</p>
-					<div class="text-lg">
-						<h3>Responsibilities</h3>
-						<ul class="list-disc list-inside">
-							<li>
-								Check our database for completion and accuracy of the Agencies in your county.
-							</li>
-							<li>
-								Perform internet research for existing Data Sources in your county, adding them to
-								our database.
-							</li>
-							<li>
-								Do in-person, phone, or email research with local experts like records clerks,
-								attorneys, journalists, activists, and municipal employees to uncover more obscure
-								Data Sources.
-							</li>
-							<li>
-								Identify topical areas of interest in local community, capturing Requests for data
-								and sharing our database where helpful.
-							</li>
-							<li>
-								When public records are not published online, make FOIA requests and
-								otherwise work with local records departments to get data published.
-							</li>
-							<li>
-								Capture and respond to Requests in your region, either by connecting Volunteers or
-								sharing relevant data.
-							</li>
-						</ul>
-						<h3>Time commitment</h3>
-						<p>
-							Flexible. There's more work to do up front for the initial research, and a slow
-							trickle of hours for maintenance and coordination over time. We will agree on a
-							schedule of work and check-ins which fits your availability.
-						</p>
-						<h3>Compensation</h3>
-						<p>$25 per hour minimum.</p>
-						<h3>Characteristics of strong applicants</h3>
-						<ul class="list-disc list-inside">
-							<li>Understands our mission, and is excited about contributing to it.</li>
-							<li>Demonstrates proficiency in web research and navigation with a browser.</li>
-							<li>
-								Is comfortable reaching out to local experts and municipal offices in-person and
-								remotely for research.
-							</li>
-							<li>
-								Has miscellaneous additional skills related to this work: data analysis, public
-								policy, web crawling, research, etc.
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div class="mt-12">
 					<h2 id="software-development-opportunities">Software development opportunities</h2>
 					<h4><i class="fa fa-info-circle"></i> Remote, temporary</h4>
 					<p>
-						We're not actively interviewing for these roles, but will keep your resume on file for
-						future work.
-					</p>
-					<p>
-						These are flexible short-term fellowships, internships, or contracts. They may last a
-						few weeks, up to 6 months depending on the project and your availability.
+						These are flexible short-term contracts meant to help us make progress on specific goals.
+						They may last from a few weeks, up to 6 months or more depending on the project and your availability.
 					</p>
 					<p>
 						We are looking for skilled applicants of any experience level to work on our software
@@ -132,14 +64,14 @@
 						</p>
 						<h3>Compensation</h3>
 						<p>
-							$25 per hour minimum. Our annual budget for this work is $50,000&ndash;100,000, scaled
-							based on experience and time commitment.
+							$25 per hour minimum, scaled based on experience and time commitment. Our annual budget for this work is $150,000&ndash;200,000.
 						</p>
 						<h3>Characteristics of strong applicants</h3>
 						<ul class="list-disc list-inside">
 							<li>Can exhibit basic knowledge of Python.</li>
 							<li>Feels excited about contributing to our mission.</li>
 							<li>Is comfortable with feedback and iteration.</li>
+							<li>Has worked on software which can be used or tested somewhere.</li>
 						</ul>
 					</div>
 				</div>
