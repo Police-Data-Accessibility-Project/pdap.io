bb595235d6788af22396a6ccf3a94c1f7cf70198
Max Chis <maxachis@gmail.com>
2025-03-26 09:48:59 -0400

    fix: Ensure see more/see less prompt behaves consistently.
    Additionally, correct bug in display of comma in County, State column for Federal data sources.

--- src/pages/data-source/[id].vue
+++ src/pages/data-source/[id].vue
@@ -90,10 +90,15 @@
                     <h4 class="m-0">County, State</h4>
                     <p>
                       {{
-                        typeof dataSource.agencies[0].county_name === 'string'
-                          ? dataSource.agencies[0].county_name
-                          : dataSource.agencies[0].county_name?.join(', ')
-                      }}, {{ dataSource.agencies[0].state_iso }}
+                        dataSource.agencies[0].county_name
+                          ? (typeof dataSource.agencies[0].county_name === 'string'
+                              ? dataSource.agencies[0].county_name
+                              : dataSource.agencies[0].county_name.join(', ')
+                          ) +
+                            (dataSource.agencies[0].state_iso ? ', ' : '')
+                          : ''
+                      }}
+                      {{ dataSource.agencies[0].state_iso || '' }}
                     </p>
                   </div>
                   <div>
@@ -220,7 +225,7 @@ import { DATA_SOURCE_UI_SHAPE } from './_util';
 import { getDataSource } from '@/api/data-sources';
 import { formatDateForSearchResults } from '@/util/dateFormatters';
 import { isDescendantOf } from '@/util/DOM';
-import { computed, onMounted, onUnmounted, ref } from 'vue';
+import { computed, onBeforeUnmount, ref, watch, watchEffect } from 'vue';
 import { useQuery } from '@tanstack/vue-query';
 import { useRoute, useRouter } from 'vue-router';
 import { useSwipe } from '@vueuse/core';
@@ -313,22 +318,35 @@ function getPrev() {
   return searchStore.mostRecentSearchIds[previousIdIndex.value];
 }
 
-onMounted(() => {
-  handleShowMoreButton();
-  window.addEventListener('resize', handleShowMoreButton);
-});
+let observer = null;
 
-onUnmounted(() => {
-  window.removeEventListener('resize', handleShowMoreButton);
-});
+watchEffect(() => {
+  if (!descriptionRef.value) return;
 
-function handleShowMoreButton() {
-  if (descriptionRef.value?.offsetHeight < descriptionRef.value?.scrollHeight) {
-    showExpandDescriptionButton.value = true;
-  } else {
-    showExpandDescriptionButton.value = false;
+  const el = descriptionRef.value;
+
+  const checkOverflow = () => {
+    showExpandDescriptionButton.value = el.offsetHeight < el.scrollHeight;
+  };
+
+  observer = new ResizeObserver(checkOverflow);
+  observer.observe(el);
+
+  // Also run on initial mount
+  checkOverflow();
+});
+watch(
+  () => route.params.id,
+  () => {
+    isDescriptionExpanded.value = false;
   }
-}
+);
+onBeforeUnmount(() => {
+  if (observer && descriptionRef.value) {
+    observer.unobserve(descriptionRef.value);
+    observer.disconnect();
+  }
+});
 
 function formatResult(record, item) {
   if (record.isDate) return formatDateForSearchResults(item);
@@ -379,10 +397,5 @@ hgroup {
 .increment-enter-from,
 .decrement-leave-to {
   transform: translateX(15%);
-}
-
-.decrement-enter-from,
-.increment-leave-to {
-  transform: translateX(-15%);
 }
 </style>
