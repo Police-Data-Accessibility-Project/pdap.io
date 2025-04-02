d35a4ecb849d3a9442bf1527b6905b1fbd833630
Joshua D. Graber <code+gh@joshuadgraber.com>
2025-04-02 17:31:56 -0400

    fix: config workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : 0,
   reporter: 'html',
   use: {
     baseURL: process.env.E2E_TESTING_URL ?? 'http://localhost:8888',
