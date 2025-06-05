2a189e1cacd82cd11a131251283f08a196a9d739
Joshua D. Graber <code+gh@joshuadgraber.com>
2025-06-05 15:09:22 -0400

    feat(api): add map/locations

---
+++ src/api/map.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+import { ENDPOINTS } from './constants';
+
+const MAP_BASE = `${import.meta.env.VITE_API_URL}/map`;
+const HEADERS_BASE = {
+  'Content-Type': 'application/json'
+};
+const HEADERS_BASIC = {
+  ...HEADERS_BASE,
+  authorization: `Basic ${import.meta.env.VITE_API_KEY}`
+};
+
+export async function getMapLocations() {
+  return await axios.get(`${MAP_BASE}/${ENDPOINTS.MAP.LOCATIONS}`, {
+    headers: HEADERS_BASIC
+  });
+}
